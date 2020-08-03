85d85808a898ad6a98e0986cc144c32e0f1e5c52
MishkaRogachev <mishkarogachev@gmail.com>
2020-08-03 10:14:26 +0300

    Add helper functions for angles

--- Industrial/Indicators/helper.js
+++ Industrial/Indicators/helper.js
@@ -1,5 +1,17 @@
 .pragma library
 
+function angle180(value) {
+    while (value <= -180) value += 360;
+    while (value > 180) value -= 360;
+    return value;
+}
+
+function angle360(value) {
+    while (value < 0) value += 360;
+    while (value >= 360) value -= 360;
+    return value;
+}
+
 function mapToRange(value, minValue, maxValue, length) {
     return (value - minValue) / (maxValue - minValue) * length;
 }
