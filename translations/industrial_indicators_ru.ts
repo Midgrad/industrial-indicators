09ade672bde76233f8d2c52026ea03a00cdfc0aa
MishkaRogachev <mishkarogachev@gmail.com>
2020-09-28 17:15:57 +0300

    Indicators translations

---
+++ translations/industrial_indicators_ru.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AttitudeDirector</name>
+    <message>
+        <location filename="../Industrial/Indicators/AttitudeDirector.qml" line="53"/>
+        <source>DISARMED</source>
+        <translation>ВЫКЛЮЧЕН</translation>
+    </message>
+</context>
+<context>
+    <name>CompassTick</name>
+    <message>
+        <location filename="../Industrial/Indicators/CompassTick.qml" line="20"/>
+        <source>N</source>
+        <translation>С</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/CompassTick.qml" line="21"/>
+        <source>E</source>
+        <translation>В</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/CompassTick.qml" line="22"/>
+        <source>S</source>
+        <translation>Ю</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/CompassTick.qml" line="23"/>
+        <source>W</source>
+        <translation>З</translation>
+    </message>
+</context>
+<context>
+    <name>DistanceLabel</name>
+    <message>
+        <location filename="../Industrial/Indicators/DistanceLabel.qml" line="7"/>
+        <source>m</source>
+        <translation>м</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/DistanceLabel.qml" line="7"/>
+        <source>km</source>
+        <translation>км</translation>
+    </message>
+</context>
+<context>
+    <name>helper</name>
+    <message>
+        <location filename="../Industrial/Indicators/helper.js" line="84"/>
+        <location filename="../Industrial/Indicators/helper.js" line="110"/>
+        <source>N/D</source>
+        <translation>Н/Д</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/helper.js" line="89"/>
+        <location filename="../Industrial/Indicators/helper.js" line="101"/>
+        <source>W</source>
+        <translation>З</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/helper.js" line="89"/>
+        <location filename="../Industrial/Indicators/helper.js" line="101"/>
+        <source>S</source>
+        <translation>Ю</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/helper.js" line="89"/>
+        <source>E</source>
+        <translation>В</translation>
+    </message>
+    <message>
+        <location filename="../Industrial/Indicators/helper.js" line="89"/>
+        <source>N</source>
+        <translation>С</translation>
+    </message>
+</context>
+</TS>
