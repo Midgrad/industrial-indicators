a57790e53b600ef99a6fa2a347541f2e7beabb4c
MishkaRogachev <mishkarogachev@gmail.com>
2020-12-08 21:45:18 +0300

    Add floor125 and ceil125 functions

--- Industrial/Indicators/helper.js
+++ Industrial/Indicators/helper.js
@@ -20,3 +20,48 @@ function mapFromRange(pos, minValue, maxValue, length) {
     return pos / length * (maxValue - minValue) + minValue;
 }
 
+function floor125(x)
+{
+    if (x === 0.0)
+        return 0.0;
+
+    var sign = (x > 0) ? 1.0 : -1.0;
+    var lx = Math.log10(Math.abs(x));
+    var p10 = Math.floor(lx);
+
+    var fr = Math.pow(10.0, lx - p10);
+    if (fr >= 10.0)
+        fr = 10.0;
+    else if (fr >= 5.0)
+        fr = 5.0;
+    else if (fr >= 2.0)
+        fr = 2.0;
+    else
+        fr = 1.0;
+
+    console.log(sign, lx, p10)
+
+    return sign * fr * Math.pow(10.0, p10);
+}
+
+function ceil125(x)
+{
+    if (x === 0.0)
+        return 0.0;
+
+    var sign = (x > 0) ? 1.0 : -1.0;
+    var lx = Math.log10(Math.abs(x));
+    var p10 = Math.floor(lx);
+
+    var fr = Math.pow(10.0, lx - p10);
+    if (fr <= 1.0)
+        fr = 1.0;
+    else if (fr <= 2.0)
+        fr = 2.0;
+    else if (fr <= 5.0)
+        fr = 5.0;
+    else
+        fr = 10.0;
+
+    return sign * fr * Math.pow(10.0, p10);
+}
